1ee50bf7ce0e35dc196bb9be3d896a9b19be7e14
abdelhakim.eloumnine <eloumnine@gmail.com>
2025-05-14 11:29:35 +0100

    Initial commit

---
+++ src/main/java/ma/bank/bcp/BanquePopulaireApplication.java
@@ -0,0 +1,13 @@
+package ma.bank.bcp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BanquePopulaireApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BanquePopulaireApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ma/bank/bcp/BanquePopulaireApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.bank.bcp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BanquePopulaireApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
